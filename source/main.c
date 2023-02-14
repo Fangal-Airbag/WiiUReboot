5eeaf2d61fab3478880fa3b3856236bf604f20d1
Fangal-Airbag <77993079+Fangal-Airbag@users.noreply.github.com>
2023-02-13 20:12:07 -0500

    Allow for full and soft reboot
    Allows for the user to fully reboot the system if B on the gamepad is held. Otherwise a soft reboot is done.

--- source/main.c
+++ source/main.c
@@ -1,11 +1,24 @@
 #include <coreinit/launch.h>
+#include <sysapp/launch.h>
+#include <vpad/input.h>
 #include <whb/proc.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+	VPADStatus status;
+	VPADReadError err;
+	
 	WHBProcInit();
 	
-	OSLaunchTitlel(OS_TITLE_ID_REBOOT, 0);
+	VPADRead(VPAD_CHAN_0, &status, 1, &err);
+
+	// To fully reboot the system the user needs to hold B
+	if (status.hold & VPAD_BUTTON_B) 
+		OSLaunchTitlel(OS_TITLE_ID_REBOOT, 0);
+	else {
+		OSForceFullRelaunch();
+		SYSLaunchMenu();
+	}
 
 	while (WHBProcIsRunning()) {}
 
